097cd50395a4f303ee860e86b349b53c0034ee8e
Samuel Bustos <samuelbuspar3221@gmail.com>
2023-12-08 04:53:25 -0500

    Create model, controllers and routes for quiz results. Tested APIs, working properly

--- server/express.js
+++ server/express.js
@@ -7,6 +7,7 @@ import helmet from 'helmet'
 import Template from './../template.js'
 import userRoutes from './routes/user.routes.js'
 import authRoutes from './routes/auth.routes.js'
+import resultsRoutes from './routes/results.routes.js';
 import path from 'path'
 
 const app = express()
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', userRoutes)
 app.use('/', authRoutes)
+app.use('/', resultsRoutes);
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
